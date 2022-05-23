dbf686a716f9a57ce4cd41084a5d8043c4540cab
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-23 10:44:54 +0300

    Initializing random variables

--- dice.js
+++ dice.js
@@ -2,3 +2,7 @@
 var h1 = document.getElementsByTagName("h1");
 var img1 = document.getElementsByClassName("img1");
 var img2 = document.getElementsByClassName("img2");
+//Random
+var dice1 = Math.floor(Math.random() * 5);
+var dice2 = Math.floor(Math.random() * 5);
+
