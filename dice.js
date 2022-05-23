dbb8acf49c3a6f719b661b3cae6c7c396c8fc334
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-05-23 10:51:32 +0300

    Adding random source to images

--- dice.js
+++ dice.js
@@ -6,3 +6,8 @@ var img2 = document.getElementsByClassName("img2");
 var dice1 = Math.floor(Math.random() * 5);
 var dice2 = Math.floor(Math.random() * 5);
 
+const img_src = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
+//Adding random source to images
+img1[0].src = img_src[dice1];
+img2[0].src = img_src[dice2];
+
